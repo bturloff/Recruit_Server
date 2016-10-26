18bdce1389c810bef80a82157cf02803b001732d
connor <maddoxconnor94@gmail.com>
2016-10-26 11:40:25 -0700

    Adding values to student schema

--- models/student.js
+++ models/student.js
@@ -3,8 +3,25 @@ var Schema = mongoose.Schema
 
 // Schema to store the school information for each recruited university
 var studentSchema = new Schema({
-  studentName: String,
+  firstName: String,
+  lastName: String,
+  emailAddress: String,
+  phoneNumber: String,
+  majorName: String,
+  degreeEnrolled: String,
   schoolName: String,
+  gradMonth: String,
+  gradYear: String,
+  hasAuthorization: String,
+  requiresVisa: String,
+  locationPreferences: String,
+  skills: String,
+  careerInterests: String,
+  positionType: String,
+  recruiterNotes: String,
+  amexRecruiterName: String,
+  getsInterview: Boolean,
+  rating: Number,
   dateAppliedUNIX: Number,
   dateApplied: String
 }, {collection: 'students'})
