e68cab8b840a78e28d46aedd0ee8410b1ac925e9
schachte <rschacht@asu.edu>
2016-11-08 22:19:52 -0700

    Added store student information

--- controllers/students.js
+++ controllers/students.js
@@ -8,6 +8,114 @@ var converter = require('json-2-csv')
 // Establish database connection
 mongoose.createConnection(commonModules.databaseConnectionString)
 
+
+/**
+ * Gets the exact student JSON data
+ * Returns all the students in JSON format
+ */
+exports.getStudentJSON = function(req, res) {
+
+  waterfall([
+        function (callback) {
+          var school = req.param('school')
+          var fromDate = req.param('from')
+          var toDate = req.param('to')
+          if (toDate != null && fromDate != null) {
+            if (school != null) {
+              studentSchema.studentData.find({'schoolName': school, 'dateAppliedUNIX': {$gte: toDate, $lt: fromDate}}, {_id: 0}, function (err, serverDbData) {
+                if (err) {
+                  res
+                    .status(commonModules.HttpStatus.INTERNAL_SERVER_ERROR)
+                    .send(err)
+                }
+                callback(null, serverDbData, 'done')
+              })
+            } else {
+              studentSchema.studentData.find({'dateAppliedUNIX': {$gte: toDate, $lt: fromDate}}, {_id: 0}, function (err, serverDbData) {
+                if (err) {
+                  res
+                    .status(commonModules.HttpStatus.INTERNAL_SERVER_ERROR)
+                    .send(err)
+                }
+                callback(null, serverDbData, 'done')
+              })
+            }
+          } else if (school != null) {
+            studentSchema.studentData.find({'schoolName': school}, {_id: 0}, function (err, serverDbData) {
+              if (err) {
+                res
+                  .status(commonModules.HttpStatus.INTERNAL_SERVER_ERROR)
+                  .send(err)
+              }
+              callback(null, serverDbData, 'done')
+            })
+          } else {
+            studentSchema.studentData.find({_id: 0}, function (err, serverDbData) {
+              if (err) {
+                res
+                  .status(commonModules.HttpStatus.INTERNAL_SERVER_ERROR)
+                  .send(err)
+              }
+              callback(null, serverDbData, 'done')
+            })
+          }
+        }
+      ], function (err, studentInformationData, result) {
+        if (err) {
+          res
+            .status(commonModules.HttpStatus.INTERNAL_SERVER_ERROR)
+            .send(err)
+        }
+
+        res.json(studentInformationData)
+      })
+}
+
+
+/**
+ * Store a specific student into the database
+ * Adds the student json from xamarin into the server
+ */
+exports.storeStudent = function (req, res) {
+    waterfall([
+      function (callback) {
+        var studentInformation = new studentSchema.studentData
+        ({"studentName": req.studentName,
+          "schoolName": req.schoolName,
+          "dateAppliedUNIX": req.dateAppliedUNIX,
+          "dateApplied": req.dateApplied,
+          "firstName": req.firstName,
+          "lastName": req.lastName,
+          "emailAddress": req.emailAddress,
+          "phoneNumber": req.phoneNumber,
+          "majorName": req.majorName,
+          "degreeEnrolled": req.degreeEnrolled,
+          "gradMonth": req.gradMonth,
+          "gradYear": req.gradYear,
+          "hasAuthorization": req.hasAuthorization,
+          "requiresVisa": req.requiresVisa,
+          "locationPreferences": req.locationPreferences,
+          "skills": req.skills,
+          "careerInterests": req.careerInterests,
+          "positionType": req.positionType,
+          "recruiterNotes": req.recruiterNotes,
+          "amexRecruiterName": req.amexRecruiterName,
+          "getsInterview": req.getsInterview,
+          "rating": req.rating
+        })
+        callback(null, studentInformation, 'done')
+      }
+    ], function (err, studentInformation, result) {
+      studentInformation.save(function (err) {
+        if (!err) {
+          res
+            .status(commonModules.HttpStatus.OK)
+            .send('Student Stored')
+        }
+      })
+    })
+}
+
 /**
  * Handle the student CSV export functonal`ity
  * Takes in a ROT64 encrypted string to parse and query the database
--- models/student.js
+++ models/student.js
@@ -5,8 +5,26 @@ var Schema = mongoose.Schema
 var studentSchema = new Schema({
   studentName: String,
   schoolName: String,
-  dateAppliedUNIX: Number,
-  dateApplied: String
+  dateAppliedUNIX: String,
+  dateApplied: String,
+  firstName: String,
+  lastName: String,
+  emailAddress: String,
+  phoneNumber: String,
+  majorName: String,
+  degreeEnrolled: String,
+  gradMonth: String,
+  gradYear: String,
+  hasAuthorization: String,
+  requiresVisa: String,
+  locationPreferences: String,
+  skills: String,
+  careerInterests: String,
+  positionType: String,
+  recruiterNotes: String,
+  amexRecruiterName: String,
+  getsInterview: String,
+  rating: String
 }, {collection: 'students'})
 
 // Assign the schema to the proper data
